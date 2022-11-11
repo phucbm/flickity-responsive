f0321d2c9e069df66846023f3b4a5001aedc479f
phucbm <hiphucbui@gmail.com>
2022-11-11 17:46:44 +0700

    :construction: option: destroy

--- src/on-matched.js
+++ src/on-matched.js
@@ -1,9 +1,16 @@
 export function onMatched(el, options){
     // get instance
     let customFlickity = Flickity.data(el);
+    const hasInitialized = typeof customFlickity !== 'undefined';
 
     // destroy if instance is found
-    if(typeof customFlickity !== 'undefined') customFlickity.destroy();
+    if(hasInitialized) customFlickity.destroy();
+
+    // option: destroy
+    if(options.destroy){
+        if(hasInitialized) customFlickity.destroy();
+        return;
+    }
 
     /** Custom script **/
     // init new instance
