a8eed4a06c7c1a145854fb27a174356a07af591c
vuquangpham <vuquangpham2909@gmail.com>
2023-01-13 16:18:29 +0700

    :zap: flickity responsive > consistent code structure

--- src/on-load.js
+++ src/on-load.js
@@ -6,8 +6,9 @@ export function onLoad(el, options){
     let flkty = Flickity.data(el);
     if(!flkty) return;
 
-    if(options.object?.wrapAround)
+    if(options.object?.wrapAround){
         options.object.wrapAround = validateWrapAround(flkty);
+    }
 
     // responsive navigation
     responsiveNavigation(flkty, options);
