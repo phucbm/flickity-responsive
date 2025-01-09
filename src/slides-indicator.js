e8bc41e1c3c4bb6b14a91a22bc717ae47211e11d
phucbm <hiphucbui@gmail.com>
2025-01-09 12:15:42 +0700

    feat(slides-indicator): allow multiple total and current text

--- src/slides-indicator.js
+++ src/slides-indicator.js
@@ -1,24 +1,24 @@
-import {getElement} from "./utils";
+import {getElements} from "./utils";
 
 export function initSlidesIndicator(flkty, options){
     const isZeroPad = options.indicatorZeroPad === true;
-    const total = getElement(options.indicatorTotal);
-    const current = getElement(options.indicatorCurrent);
+    let total = getElements(options.indicatorTotal);
+    let current = getElements(options.indicatorCurrent);
     const totalSlides = flkty.slides.length;
 
     // total
     if(total){
-        total.innerText = isZeroPad ? getNumber(totalSlides) : totalSlides;
+        total.forEach(el => el.innerText = isZeroPad ? getNumber(totalSlides) : totalSlides);
     }
 
     // current
     if(current){
         let index = flkty.selectedIndex;
-        current.innerText = isZeroPad ? getNumber(index + 1) : index + 1;
+        current.forEach(el => el.innerText = isZeroPad ? getNumber(index + 1) : index + 1);
 
         flkty.on('change', (index) => {
             const selectedNumber = flkty.selectedIndex + 1;
-            current.innerText = isZeroPad ? selectedNumber.toString().padStart(2, '0') : selectedNumber;
+            current.forEach(el => el.innerText = isZeroPad ? getNumber(selectedNumber) : selectedNumber);
         });
     }
 }
